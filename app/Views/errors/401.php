1de65f05b0d13798ea29f6939d06a42e80af594f
Michael <8michael8e@gmail.com>
2025-02-10 17:50:57 +0200

    Delete app directory

--- app/Views/errors/401.php
+++
@@ -1,5 +0,0 @@
-<h1>401 - Unauthorized</h1>
-<p>You're not authorized to do this.</p>
-<p>
-  <a href="/login">Return to login page</a>
-</p>
--- app/Views/errors/419.php
+++
@@ -1,5 +0,0 @@
-<h1>419 - Page Expired</h1>
-<p>You probably took to long to submit a form. Sorry and please try again (be quicker!).</p>
-<p>
-  <a href="/">Return to homepage</a>
-</p>
